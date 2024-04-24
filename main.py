23828d40f6be70ea84fe280a0fdb14621783d01a
DonM744 <164878944+DonM744@users.noreply.github.com>
2024-04-24 13:31:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+# letzter Stand
+
 import shutil
 from flask import Flask
-from flask import render_template_string
+from flask import render_template_string, redirect, request
 
 import os
 import subprocess
@@ -8,13 +10,11 @@ import socket
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return render_template_string("""
-    <!DOCTYPE html>
+nav_bar = """
+ <!DOCTYPE html>
     <html>
     <head>
-        <title>Startseite</title>
+        <title>Nachhilfemanager</title>
         <style>
             nav ul {
                 list-style-type: none;
@@ -42,70 +42,34 @@ def home():
         </style>
     </head>
     <body>
-        <h1>Startseite</h1>
+        <h1>Nachhilfemanager 🏫</h1>
         <nav>
             <ul>
                 <li><a href="/">Home</a></li>
                 <li><a href="/kontakte">Kontakte</a></li>
                 <li><a href="/dateien">Dateien</a></li>
+                <li><a href="/forum">Forum</a></li>
             </ul>
         </nav>
     </body>
     </html>
-    """)
+"""
+
+@app.route('/')
+def home():
+    return nav_bar
 
 @app.route('/kontakte')
 def kontakte():
-    return render_template_string("""
-    <!DOCTYPE html>
-    <html>
-    <head>
-        <title>Kontakte</title>
-        <style>
-            nav ul {
-                list-style-type: none;
-                margin: 0;
-                padding: 0;
-                overflow: hidden;
-                background-color: #333;
-            }
+    return render_template_string(nav_bar + """
 
-            nav li {
-                float: left;
-            }
-
-            nav li a {
-                display: block;
-                color: white;
-                text-align: center;
-                padding: 14px 16px;
-                text-decoration: none;
-            }
-
-            nav li a:hover {
-                background-color: #ddd;
-                color: black;
-            }
-        </style>
-    </head>
-    <body>
-        <h1>Kontakte</h1>
-        <nav>
-            <ul>
-                <li><a href="/">Home</a></li>
-                <li><a href="/kontakte">Kontakte</a></li>
-                <li><a href="/dateien">Dateien</a></li>
-            </ul>
-        </nav>
-    </body>
-    <h3>Lehrer:</h1>
-
+    <h3>Lehrer</h3>
 
 <table>
   <tr>
-    <th>Herr Streng </th>
-    <th>Herr Weigel </th>
-    <th>Frau Bauernfeind </th>
+    <th>Herr ... </th>
+    <th>Herr ... </th>
+    <th>Frau ... </th>
 
     
   </tr>
@@ -126,66 +90,18 @@ def kontakte():
 
 @app.route('/dateien')
 def root():
-    return render_template_string('''
-    <!DOCTYPE html>
-    <html>
-    <head>
-        <title>Dateien</title>
-        <style>
-            nav ul {
-                list-style-type: none;
-                margin: 0;
-                padding: 0;
-                overflow: hidden;
-                background-color: #333;
-            }
-
-            nav li {
-                float: left;
-            }
-
-            nav li a {
-                display: block;
-                color: white;
-                text-align: center;
-                padding: 14px 16px;
-                text-decoration: none;
-            }
-
-            nav li a:hover {
-                background-color: #ddd;
-                color: black;
-            }
-        </style>
-    </head>
-    <body>
-        <h1>Dateien</h1>
-        <nav>
-            <ul>
-                <li><a href="/">Home</a></li>
-                <li><a href="/kontakte">Kontakte</a></li>
-                <li><a href="/dateien">Dateien</a></li>
-            </ul>
-        </nav>
-    </body>
-    </html>
-    
+    return render_template_string(nav_bar + '''
         <html>
           <head>
-            <h3>Dateien</h3>
+            <h3>Lernmaterialien</h3>
           </head>
           <body>
-           <p align = "center"> <Strong> Aktuelles Verzeichnis:</Strong> {{current_path}}</p>
+           <p align = "center"> <Strong> Aktuelles Verzeichnis:</Strong> {{show_current_path}}</p>
            <ul>
-           <form action="/md">
-                <input type="submit" value="Erstellen"/>
-                <input name="folder" type="text" value= "Neuer Ordner "/>
-              </form>
-           <li><a href="/back"> ← </a><li>
            {% for item in file_list %}
               {% if "." not in item%}
-              <li><strong><a href="/cd?path={{current_path + '/' + item}}">{{item}}</a></strong><a href="/rm?dir={{item}}">  🗑</a></li>
-              {% elif '.txt' or  '.json' or '.html' in item %}f
+               <li><strong><a href="/cd?path={{current_path + '/' + item}}">{{item}}</a></strong> </li>
+              {% elif '.txt' or  '.json' or '.html' in item %}
               <li><strong><a href = "/view?file={{current_path + '/' + item +'.txt'}}">{{item}}</a></strong></li>
               {% else %}
               <li>{{item}}</li>
@@ -193,13 +109,29 @@ def root():
            {%endfor%}
           </body>
         </html>
-    ''', current_path = os.getcwd(),
+    ''', show_current_path = os.getcwd().replace('/Users/jonas/PycharmProjects/Nachhilfe Manager', '/'),
+        current_path = os.getcwd(),
+
          file_list = subprocess.check_output('ls', shell=True, cwd= os.getcwd()).decode('utf8').split('\n',))
 
-@app.route('/back')
-def back():
-    os.chdir('..')
-    return redirect('/')
+@app.route('/forum')
+def forum():
+    return render_template_string(nav_bar + """
+<h3> Forum </h3>
+<form action="/action_page.php">
+  <form action="/action_page.php">
+  <p><label for="w3review">Nachricht:</label></p>
+  <textarea id="w3review" name="message" rows="4" cols="50"></textarea>
+  <br>
+  <input type="submit" value="Senden ✉️">
+</form>
+
+
+
+</body>
+</html>
+
+""")
 
 @app.route('/cd')
 def cd():
@@ -209,22 +141,11 @@ def cd():
 @app.route('/view')
 def view():
     return subprocess.check_output('cat ' + request.args.get('file'), shell=True).decode('utf-8').replace('n', '<br>')
-
-@app.route('/md')
-def md():
-    folder_name = request.args.get('folder')
-    if folder_name not in os.listdir(os.getcwd()):
-        os.mkdir(folder_name)
-    else:
-        print('Ordnername existiert bereits!')
-    return redirect('/')
-
-@app.route('/rm')
-def rm():
-    shutil.rmtree(os.getcwd() + '/' + request.args.get('dir'))
-    return redirect('/')
+# nur für eingeloggte nutzer
 
 
+# später zu socket server wechseln
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=3000)
+
